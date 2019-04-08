b3206c6f5823f95c3e7e5d907df82f5ceb8903ae
Theo <theoleanse@gmail.com>
2019-04-08 18:07:23 +0100

    add gatsby-plugin-netlify (#10)

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-netlify-cms'
+    'gatsby-plugin-netlify-cms',
+    `gatsby-plugin-netlify`
   ]
 }
